480383976edbe3787d3fa98780de8f1a1c9d9335
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-28 23:14:32 -0700

    Implemented ArrayList

--- ArrayList.java
+++ ArrayList.java
@@ -1,28 +1,65 @@
+import java.util.Arrays;
 
 public class ArrayList<T> implements List<T> {
+	private T[] arr;
+	private int size;
+	private final int DEFAULT_SIZE = 10;
+	
+	public ArrayList() {
+		arr = (T[]) new Object[DEFAULT_SIZE];
+		size = 0;
+	}
 
 	public void add(T item) {
-		// TODO Auto-generated method stub
+		if (size == arr.length) {
+			arr = Arrays.copyOf(arr, arr.length * 2);
+		}
 		
+		arr[size++] = item;
 	}
 
 	public void add(int pos, T item) {
-		// TODO Auto-generated method stub
+		if (pos > size || pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
 		
+		if (size == arr.length) {
+			arr = Arrays.copyOf(arr, arr.length * 2);
+		}
+		
+		for (int i = size - 1; i >= pos; i--) {
+			arr[i + 1] = arr[i];
+		}
+		
+		arr[pos] = item;
+		size++;
 	}
 
 	public T get(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos >= size || pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		return arr[pos];
 	}
 
 	public T remove(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos >= size|| pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		T item = arr[pos];
+		
+		for (int i = pos + 1; i < size; i++) {
+			arr[i - 1] = arr[i];
+		}
+		
+		size--;
+		return item;
+		
 	}
 
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 }

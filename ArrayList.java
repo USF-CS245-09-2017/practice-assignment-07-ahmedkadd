fd08e466973741bb47b1f6ee8fc1f22184b38e6e
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-28 22:34:40 -0700

    Created ArrayList class

---
+++ ArrayList.java
@@ -0,0 +1,28 @@
+
+public class ArrayList<T> implements List<T> {
+
+	public void add(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void add(int pos, T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public T get(int pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public T remove(int pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}

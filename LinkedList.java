26877b372875c352d8ab382de1406dda0d8a36d8
ahmedkadd <ahmedkaddoura99@gmail.com>
2019-03-28 23:41:11 -0700

    Completed assignment

--- LinkedList.java
+++ LinkedList.java
@@ -1,28 +1,88 @@
 
 public class LinkedList<T> implements List<T> {
+	private Node head;
+	private int size;
+	
+	private class Node<T> {
+		private T data;
+		private Node next;
+		
+		Node(T data) {
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	public LinkedList() {
+		this.head = new Node(null);
+		size = 0;
+	}
 
 	public void add(T item) {
-		// TODO Auto-generated method stub
+		Node newNode = new Node(item);
 		
+		Node end = head;
+		
+		while (end.next != null) {
+			end = end.next;
+		}
+		
+		end.next = newNode;
+		size++;
 	}
 
 	public void add(int pos, T item) {
-		// TODO Auto-generated method stub
+		if (pos > size || pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
 		
+		Node newNode = new Node(item);
+		
+		Node previous = head;
+		
+		for (int i = 0; i < pos - 1; i++) {
+			previous = previous.next;
+		}
+		
+		newNode.next = previous.next;
+		previous.next = newNode;
+
+		size++;
 	}
 
 	public T get(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos >= size || pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		Node node = head;
+		
+		for (int i = 0; i < pos + 1; i++) {
+			node = node.next;
+		}
+		
+		return (T) node.data;
 	}
 
 	public T remove(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos >= size || pos < 0) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		Node previous = head;
+		
+		for (int i = 0; i < pos - 1; i++) {
+			previous = previous.next;
+		}
+		
+		T item = (T) previous.next.data;
+		previous.next = previous.next.next;
+		
+		size--;
+		return item;
 	}
 
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 }
